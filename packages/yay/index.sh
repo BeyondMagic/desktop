57e51379f1b43d9221bdf10c9fb18764728edf92
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-01-10 15:25:22 -0300

    refactor(yay): use dash for managing yay-bin package

---
+++ packages/yay/index.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env dash
+#
+# João V. Farias © BeyondMagic 2026 <beyondmagic@mail.ru>
+
+set -eu
+
+NAME="yay-bin"
+DIR="/tmp/packages/yay"
+REMOTE="https://aur.archlinux.org/yay-bin.git"
+
+clone() {
+    doas rm -rf "$DIR"
+    git clone --recurse-submodules "$REMOTE" "$DIR"
+}
+
+build() {
+    cd "$DIR" || exit 1
+    makepkg -si --noconfirm
+}
+
+install() {
+    # Alias to build (kept for parity)
+    build
+}
+
+uninstall() {
+    doas pacman -Rn "$NAME"
+}
+
+cmd="${1:-}"
+case "$cmd" in
+    clone)     clone ;;
+    build)     build ;;
+    install)   install ;;
+    uninstall) uninstall ;;
+    *) echo "usage: $0 {clone|build|install|uninstall}" >&2; exit 1 ;;
+esac
