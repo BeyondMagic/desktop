e8410a6105afcd8049fbcefd3947dd9c6bda9dcd
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-01-22 15:07:02 -0300

    fix(autocmd): improve error handling for module loading

--- config/neovim/lua/autocmd.lua
+++ config/neovim/lua/autocmd.lua
@@ -10,7 +10,15 @@ for _, module_name in ipairs(modules) do
 	local ok, module = pcall(require, module_name)
 
 	if not ok then
-		vim.api.nvim_err_writeln(('Failed to load autocmd module: %s'):format(module_name))
+		vim.api.nvim_echo(
+			{
+				{('Failed to load autocmd module: %s').format(module_name)}
+			},
+			true,
+			{
+				err=true
+			}
+		)
 	else
 		local setup = module and module.setup
 		if type(setup) == 'function' then
