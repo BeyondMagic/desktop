ad3e604a75f71a4480966eedeb7f238098ab8a13
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-11 00:59:17 -0300

    feat(calendar): add Google Calendar event fetching function

---
+++ config/ags/app/widget/bar/date/google.ts
@@ -0,0 +1,29 @@
+import { execAsync } from "ags/process";
+
+export default async function ({
+	time_min,
+	time_max
+}: {
+	time_min: string;
+	time_max: string;
+}) {
+
+	const stdout = await execAsync([
+		"calendar.nu",
+		"main",
+		"events",
+		"--time-min", time_min,
+		"--time-max", time_max,
+		"--json=true"
+	]);
+
+	const events = JSON.parse(stdout);
+
+	return events.map((event: any) => ({
+		title: event.summary,
+		start: new Date(event.start),
+		end: new Date(event.end),
+		id: event.id,
+		type: event.event_type
+	}));
+}
