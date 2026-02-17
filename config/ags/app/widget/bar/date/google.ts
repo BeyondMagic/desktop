c4e8291691860057024685c6daf0670aa04b2ee5
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-02-17 02:19:57 -0300

    feat(google): add calendar parameter to events function for improved flexibility

--- config/ags/app/widget/bar/date/google.ts
+++ config/ags/app/widget/bar/date/google.ts
@@ -2,15 +2,18 @@ import { execAsync } from "ags/process";
 
 export async function events({
 	time_min,
-	time_max
+	time_max,
+	calendar = "primary"
 }: {
 	time_min: string;
 	time_max: string;
+	calendar?: string;
 }) {
 
 	const stdout = await execAsync([
 		"calendar3.nu",
 		"events",
+		"--calendar", calendar,
 		"--time-min", time_min,
 		"--time-max", time_max,
 		"--json=true"
