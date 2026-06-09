43ae11e1608fd5ce94e6df7eeae8d08ba6615c92
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-06-09 00:09:55 -0300

    feat(haskell): add Haskell buffer settings for indentation and tab handling

---
+++ config/neovim/after/ftplugin/haskell.lua
@@ -0,0 +1,5 @@
+-- Use vim.opt_local so settings only apply to the current Haskell buffer
+vim.opt_local.expandtab = true -- Convert tabs to spaces
+vim.opt_local.shiftwidth = 2   -- Number of spaces for each step of indent
+vim.opt_local.tabstop = 2      -- Number of spaces a tab counts for
+vim.opt_local.softtabstop = 2  -- Number of spaces a tab counts for while editing
