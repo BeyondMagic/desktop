805e23d90b46829813609e7c3957b26a101ce65c
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-06-15 01:58:57 -0300

    fix(monitor): update eDP-1 settings and comment out HDMI-A-1 configuration

--- config/hypr/monitor/monitor-laptop.lua
+++ config/hypr/monitor/monitor-laptop.lua
@@ -4,5 +4,5 @@
 -- Monitors
 -- --------------------------------------------------------------------------
 
-hl.monitor({ output = "DP-1", mode = "1920x1080@74.97Hz", position = "0x0", scale = 1, sdr_eotf = 1 })
-hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@75.00Hz", position = "1920x0", scale = 1, sdr_eotf = 1 })
+hl.monitor({ output = "eDP-1", mode = "1920x1080@60.00Hz", position = "0x0", scale = 1, sdr_eotf = 1 })
+-- hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@75.00Hz", position = "1920x0", scale = 1, sdr_eotf = 1 })
