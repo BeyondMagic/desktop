006a537f482be862f661e16af35625d0cee41b1c
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-06-25 22:09:24 -0300

    refactor(lsp): switch from ccls to clangd for improved language support delete(plugins): remove ccls.nvim plugin configuration

--- config/neovim/lua/lsp/c_c++.lua
+++ config/neovim/lua/lsp/c_c++.lua
@@ -1,25 +1,4 @@
 -- local util = require "lspconfig.util"
 ---@diagnostic disable: undefined-global
 
-vim.lsp.config("ccls", {
-	init_options = {
-		compilationDatabaseDirectory = "build",
-		index = {
-			threads = 0,
-		},
-		cache = {
-			directory = vim.fs.normalize "~/.cache/ccls/",
-		},
-		-- on_attach = function(client, bufnr)
-		-- 	if client.server_capabilities.documentSymbolProvider then
-		-- 		require("nvim-navic").attach(client, bufnr)
-		-- 	end
-		-- end,
-		-- clang = {
-		-- 	includeArgs = { "-I/usr/include", "-I/usr/local/include" },
-		-- 	excludeArgs = { "-frounding-math" },
-		-- },
-	}
-})
-
-vim.lsp.enable("ccls")
+vim.lsp.enable('clangd')
